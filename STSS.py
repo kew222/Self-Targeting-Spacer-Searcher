777cb4e9e5b5ad78ba59661e407d803e0b10adde
Kyle Watters <watters@berkeley.edu>
2018-05-22 10:44:53 -0700

    changing get_Accs to prevent error when no Accs are returned

--- STSS.py
+++ STSS.py
@@ -592,17 +592,18 @@ def gather_assemblies_from_bioproject_IDs(bioprojectIDs,IDs=True,num_limit=10000
 
 def get_Accs(IDs):
     
+    Accs = []
     attempt = 1
     while attempt < 4:
         try:    
             handle = Entrez.efetch(db='nuccore', rettype="acc", id=IDs)   #Get Acc#s of those found from search
-            Accs = []
             for Id in handle:
                 if Id.strip() != "":
                     Accs.append(Id.strip())          
             handle.close()
             break
-        except:
+        except Error as e:
+            print(e)
             if attempt < 4:
                 attempt += 1
             else:
